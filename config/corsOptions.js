7b8c18f5a798af8de4c4904ea7b3cc19e9934cb1
Rakibulislam-emon <rakibulislamemon60>
2025-05-12 00:13:17 +0600

    production url seted as cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,8 @@
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://github-repo-remover-node.vercel.app",
+  ],
   methods: ["GET", "POST", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
