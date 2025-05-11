ae79dbcacdfc8b29fa423f683a7eea79e433b744
Rakibulislam-emon <rakibulislamemon60>
2025-05-12 00:26:28 +0600

    Fix cors url

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://github-repo-remover-node.vercel.app",
+    "https://github-repo-remover.vercel.app",
   ],
   methods: ["GET", "POST", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
